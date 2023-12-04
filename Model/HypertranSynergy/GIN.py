ce02a9ac8f55bb39ca87049fbd4c79682d80832a
yuangaolin <55375083+yuangaolin@users.noreply.github.com>
2023-12-04 12:40:50 +0800

    Update GIN.py

--- Model/HypertranSynergy/GIN.py
+++ Model/HypertranSynergy/GIN.py
@@ -46,6 +46,6 @@ class GNN_drug(torch.nn.Module):
 
         node_representation = self.JK(x_drug_list)
         x_drug = global_max_pool(node_representation, batch)
-        x_drug = x_drug[:,:100]
+        x_drug = x_drug[:,:128]
 
         return x_drug
