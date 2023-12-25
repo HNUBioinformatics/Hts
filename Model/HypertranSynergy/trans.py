e6d99247df5c7a4dfd4f36cf1993702b036ea510
yuangaolin <55375083+yuangaolin@users.noreply.github.com>
2023-12-25 17:46:18 +0800

    Update trans.py

--- Model/HypertranSynergy/trans.py
+++ Model/HypertranSynergy/trans.py
@@ -1,53 +1,42 @@
-# -*- coding: utf-8 -*-
-# @Time   : 2020/6/27 16:40
-# @Author : Shanlei Mu
-# @Email  : slmu@ruc.edu.cn
-# @File   : layers.py
-
-# UPDATE:
-# @Time   : 2020/8/24 14:58, 2020/9/16, 2020/9/21, 2020/10/9
-# @Author : Yujie Lu, Xingyu Pan, Zhichao Feng, Hui Wang
-# @Email  : yujielu1998@gmail.com, panxy@ruc.edu.cn, fzcbupt@gmail.com, hui.wang@ruc.edu.cn
-
 import math
 import torch.nn.functional as fn
-
 from contranorm import *
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads, hidden_size, hidden_dropout_prob, attn_dropout_prob, layer_norm_eps):
+    def __init__(self, heads, size, dp1, dp2, ne):
         super(MultiHeadAttention, self).__init__()
-        if hidden_size % n_heads != 0:
+        if size % heads != 0:
             raise ValueError(
                 "The hidden size (%d) is not a multiple of the number of attention "
-                "heads (%d)" % (hidden_size, n_heads)
+                "heads (%d)" % (size, heads)
             )
 
-        self.num_attention_heads = n_heads
-        self.attention_head_size = int(hidden_size / n_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
+        self.heads = heads
+        self.h_size = int(size / heads)
+        self.t_size = self.heads * self.h_size
 
-        self.query = nn.Linear(hidden_size, self.all_head_size)
-        self.key = nn.Linear(hidden_size, self.all_head_size)
-        self.value = nn.Linear(hidden_size, self.all_head_size)
+        self.q = nn.Linear(size, self.t_size)
+        self.k = nn.Linear(size, self.t_size)
+        self.v = nn.Linear(size, self.t_size)
 
-        self.attn_dropout = nn.Dropout(attn_dropout_prob)
+        self.attn_dropout = nn.Dropout(dp2)
 
-        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.dense = nn.Linear(size, size)
 
-        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
-        self.LayerNorm1 = ContraNorm(hidden_size, scale=0.0, dual_norm=False, pre_norm=False, temp=1.0, learnable=False,
+        self.LayerNorm = nn.LayerNorm(size, eps=ne)
+        self.LayerNorm1 = ContraNorm(size, scale=0.0, dual_norm=False, pre_norm=False, temp=1.0, learnable=False,
                                      positive=False, identity=False)
         self.input_proj = nn.Linear(50, 142)
 
-        self.out_dropout = nn.Dropout(hidden_dropout_prob)
+        self.out_dropout = nn.Dropout(dp1)
 
     def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        new_x_shape = x.size()[:-1] + (self.heads, self.h_size)
         x = x.view(*new_x_shape)
         return x.permute(0, 2, 1)
 
     def forward(self, input_tensor, attention_mask):
-        mixed_query_layer = self.query(input_tensor)
+        mixed_query_layer = self.q(input_tensor)
         mixed_key_layer = self.key(input_tensor)
         mixed_value_layer = self.value(input_tensor)
 
@@ -61,7 +50,7 @@ class MultiHeadAttention(nn.Module):
         attention_scores = attention_scores + noise * 40
 
 
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = attention_scores / math.sqrt(self.h_size)
 
         if attention_mask is not None:
             attention_scores = attention_scores + attention_mask
@@ -72,7 +61,7 @@ class MultiHeadAttention(nn.Module):
         attention_probs = self.attn_dropout(attention_probs)
         context_layer = torch.matmul(attention_probs, value_layer)
         context_layer = context_layer.permute(0, 2, 1).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        new_context_layer_shape = context_layer.size()[:-2] + (self.t_size,)
         context_layer = context_layer.view(*new_context_layer_shape)
         hidden_states = self.dense(context_layer)
         hidden_states = self.out_dropout(hidden_states)
