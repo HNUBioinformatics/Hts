8c150eeb2997beba9b16ae94a0d6ad232a488563
yuangaolin <55375083+yuangaolin@users.noreply.github.com>
2023-12-25 17:12:17 +0800

    Update GIN.py

--- Model/HypertranSynergy/GIN.py
+++ Model/HypertranSynergy/GIN.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 from torch_geometric.nn import GINConv, GCNConv, JumpingKnowledge, global_max_pool
 
 class GIN_drug(torch.nn.Module):
-    def __init__(self, layer_drug, dim_drug):
+    def __init__(self, nn1, dim):
         super().__init__()
-        self.layer_drug = layer_drug
-        self.dim_drug = dim_drug
+        self.nn1 = nn1
+        self.dim = dim
         self.JK = JumpingKnowledge('cat')
         self.convs_drug = torch.nn.ModuleList()
         self.bns_drug = torch.nn.ModuleList()
@@ -16,14 +16,14 @@ class GIN_drug(torch.nn.Module):
 
         self.dropout = nn.Dropout(0.3)
 
-        for i in range(self.layer_drug):
+        for i in range(self.nn1):
             if i:
-                block = nn.Sequential(nn.Linear(self.dim_drug, self.dim_drug), nn.ReLU(),
-                                      nn.Linear(self.dim_drug, self.dim_drug))
+                block = nn.Sequential(nn.Linear(self.dim, self.dim), nn.ReLU(),
+                                      nn.Linear(self.dim, self.dim))
             else:
-                block = nn.Sequential(nn.Linear(75, self.dim_drug), nn.ReLU(), nn.Linear(self.dim_drug, self.dim_drug))
+                block = nn.Sequential(nn.Linear(75, self.dim), nn.ReLU(), nn.Linear(self.dim, self.dim))
             conv = GINConv(block)
-            bn = torch.nn.BatchNorm1d(self.dim_drug)
+            bn = torch.nn.BatchNorm1d(self.dim)
 
             self.convs_drug.append(conv)
             self.bns_drug.append(bn)
@@ -31,7 +31,7 @@ class GIN_drug(torch.nn.Module):
     def forward(self, drug_feature, drug_adj, ibatch):
         x, edge_index, batch = drug_feature, drug_adj, ibatch
         x_drug_list = []
-        for i in range(self.layer_drug):
+        for i in range(self.nn1):
             x = F.relu(self.dropout(self.convs_drug[i](x, edge_index)))
 
             # x = self.dropout(x)
