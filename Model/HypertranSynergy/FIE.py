ecb0e09265f1c63511c3e54b497f80ba13dd6c7d
yuangaolin <55375083+yuangaolin@users.noreply.github.com>
2024-01-08 16:40:43 +0800

    Update and rename FIE to FIE.py

---
+++ Model/HypertranSynergy/FIE.py
@@ -0,0 +1,13 @@
+﻿import numpy as np
+from scipy.stats import pearsonr
+import sys
+sys.path.append('')
+
+from FIE1 import *
+
+def att_sim(interaction_matrix):
+    X = ATT_learner(2,np.array(interaction_matrix).shape[1],30,'cosine',6,0,'relu')
+    X.forward = (interaction_matrix)
+    result = X.forward
+    #result = np.array(result1).reshape(87,87)
+    return result
